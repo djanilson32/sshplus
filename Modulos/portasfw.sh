e0238eba538d7e5d9be28161ddb4701f059ea43c
UM NOOB <69488194+RPVIP@users.noreply.github.com>
2022-04-18 18:22:16 -0300

    Update portasfw.sh

--- Modulos/portasfw.sh
+++ Modulos/portasfw.sh
@@ -2,7 +2,7 @@
 
 echo -e "\033[1;31mATENCAO!!\033[0m"
 echo " "
-echo -e "\033[1;33mEssas script ira liberar as portas 80 8080 1194 81 443 442 servidor
+echo -e "\033[1;33mEssas script ira liberar as portas 80 8080 8799 1194 81 443 442 servidor
 \033[0m"
 echo""
 echo ""
@@ -20,6 +20,7 @@ sudo firewall-cmd --zone=public --permanent --add-port=1194/tcp &&
 sudo firewall-cmd --zone=public --permanent --add-port=443/tcp &&
 sudo firewall-cmd --zone=public --permanent --add-port=81/tcp &&
 sudo firewall-cmd --zone=public --permanent --add-port=442/tcp &&
+sudo firewall-cmd --zone=public --permanent --add-port=8799/tcp &&
 sudo firewall-cmd --reload &&
 sudo firewall-cmd --zone=public --list-ports
 
